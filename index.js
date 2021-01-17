c55c34e635e2ef88ff0b39bbdcfb3ee39197f6ec
abderrahim <info.abderrahimtijani@gmail.com>
2021-01-17 16:37:27 +0100

    Add stuff for heroku

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 8080
 
 
 app.use(express.static('public'))
@@ -10,6 +9,4 @@ app.get('/', (req, res) => {
   res.send('Hello abdo i am your first app!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || 8080)
